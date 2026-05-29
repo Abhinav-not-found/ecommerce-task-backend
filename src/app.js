7fac15b14fa47b64ecf75ef4d9c422f52a3334a8
abhinav <abhinavisgreat18@gmail.com>
2026-05-29 12:13:01 +0530

    feat: added auth route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import express from "express";
-// import authRoute from "./";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
-
+//middlewares
 app.use(express.json());
 
-// app.use("/api/auth", authRoute);
+// health check
+app.get("/", (_, res) => {
+	res.send("Ecommerce backend is running");
+});
+
+// routes
+app.use("/api/auth", authRoute);
+
+// global error handler
+app.use((err, _, res, __) => {
+	const statusCode = err.statusCode || 500;
+	const errorMessage = err.message || "Internal server error";
+
+	return res.status(statusCode).json({
+		message: errorMessage,
+	});
+});
 
 export default app;
