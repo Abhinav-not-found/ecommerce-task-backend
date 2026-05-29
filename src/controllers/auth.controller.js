b0e953b1366bcd4befe319bfe4f5d1fc14cf2456
abhinav <abhinavisgreat18@gmail.com>
2026-05-29 12:39:28 +0530

    feat: added apiError and apiResponse

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,17 @@
+import Auth from "../models/auth.model.js";
 import { registerValidator } from "../validators/auth.validator.js";
 
 export const register = async (req, res, next) => {
 	try {
 		const { name, email, password } = await registerValidator(req);
+		//check if email already exist
+		const checkUserAlreadyExist = await Auth;
+
+    
+		// make new user
+		// create token
+		// set cookies
+		// send res
 	} catch (error) {
 		next(error);
 	}
